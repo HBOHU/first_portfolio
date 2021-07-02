8d1164ac35393b9b0b0ea6a9a3d2697acfb758cf
peacock | mac <stijnvdpouw@gmail.com>
2021-07-02 23:12:12 +0200

    mailJS working

--- assets/js/script.js
+++ assets/js/script.js
@@ -80,7 +80,26 @@ $(function () {
 
 // Mail form
 //------------------------------------------------------------------------------------------------------------------------
+function sendMail() {
+    var tempParams = {
+        from_name : document.getElementById("name").value,
+        from_mail : document.getElementById("mail").value,
+        message : document.getElementById("message").value,
+    };
+    emailjs.send('service_icap7vg', 'template_siyx4gr', tempParams)
+    .then(function(res){
+        $(':input','#mail_form')
+        .not(':button, :submit, :reset, :hidden')
+        .val('');
+        if (res.status == 200){
+            console.log("succes", res.status);
+        }
+        else {
+            // not succes
+        }
 
+    });
+};
 
 // TypeWriter
 class TypeWriter {
