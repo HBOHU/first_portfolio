8ee03f88b28143e1741c1b730bd1c14f34fea4f2
BOTjoker-ops <stijnvdpouw@gmail.com>
2021-07-10 15:17:13 +0200

    rename

---
+++ assets/js/main.js
@@ -0,0 +1,73 @@
+$(function () {
+    $('.navbar-toggle').click(function () {
+        $(this).toggleClass('act');
+        if ($(this).hasClass('act')) {
+            $('.main-menu').addClass('act');
+        } else {
+            $('.main-menu').removeClass('act');
+        }
+    });
+
+    //jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(document).on('click', '.page-scroll a', function (event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1400, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.site-header',
+        offset: 5
+    });
+
+    /* Skill bars */
+    $(".btn:not(.active)").click(function () {
+        $(".progress-bar").addClass("notransition");
+        $('.progress-bar').attr('style', "width: 0%");
+    });
+
+    $(".btn").click(function () {
+        $('.progress-bar').progressbar({
+            transition_delay: 40
+        });
+    });
+
+    /* Counters  */
+    if ($(".section-counters .start").length > 0) {
+        $(".section-counters .start").each(function () {
+            var stat_item = $(this),
+                offset = stat_item.offset().top;
+            $(window).scroll(function () {
+                if ($(window).scrollTop() > (offset - 1000) && !(stat_item.hasClass('counting'))) {
+                    stat_item.addClass('counting');
+                    stat_item.countTo();
+                }
+            });
+        });
+    };
+
+    // another custom callback for counting to infinity
+    $('#infinity').data('countToOptions', {
+        onComplete: function (value) {
+            count.call(this, {
+                from: value,
+                to: value + 1
+            });
+        }
+    });
+
+    $('#infinity').each(count);
+
+    function count(options) {
+        var $this = $(this);
+        options = $.extend({}, options || {}, $this.data('countToOptions') || {});
+        $this.countTo(options);
+    }
+    
+
+
+    
+});
